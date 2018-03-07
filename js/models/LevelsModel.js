f15ac6194ee9386e12f10996ea3f4817fed1929f
BDeliers <contact@bdeliers.com>
2018-03-07 12:12:42 +0100

    Added LevelsModel.js and deleted pour_balth.js

---
+++ js/models/LevelsModel.js
@@ -0,0 +1,48 @@
+//Levels management class
+
+var Levels = function() {
+    this.pollution_level = 0;
+    this.money_level = 100;
+    this.exhaust_level = 0;
+    this.time_level = 0;
+};
+
+Levels.prototype.updateLevels = function(vehicle, active_events) {
+    var pollution_coeff = vehicle.pollutionCoeff();
+    var money_coeff = vehicle.moneyCoeff();
+    var exhaust_coeff = vehicle.exhaustCoeff();
+    var time_coeff = vehicle.timeCoeff();
+
+    for(event in active_events){
+        pollution_coeff *= (1 + event.perturbativePollutionCoeff());
+        money_coeff *+= (1 + event.perturbativeMoneyCoeff());
+        exhaust_coeff *= (1 + event.perturbativeExhaustCoeff());
+        time_coeff *= (1 + event.perturbativeTimeCoeff());
+    }
+
+    this.pollution_level += 33 * pollutionCoeff;
+    this.money_level -= 33 * money_coeff;
+    this.exhaust_level += 33 * exhaust_coeff;
+    this.time_level += 33 * time_coeff;
+}
+
+Levels.prototype.getPollutionLevel = function() {
+    return this.pollution_level % 101;
+}
+
+Levels.prototype.getMoneyLevel = function() {
+    return this.money_level % 101;
+}
+
+Levels.prototype.getExhaustLevel = function() {
+    if (this.exhaust_level < 0) {
+        return 0;
+    }
+    else {
+        return this.exhaust_level;
+    }
+}
+
+Levels.prototype.getTimeLevel = function() {
+    return this.time_level;
+}
