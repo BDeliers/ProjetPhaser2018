29675774beb2a2867b01fcc68219b69a1051d619
BDeliers <contact@bdeliers.com>
2018-03-08 19:54:15 +0100

    Improved and simplified Gauge module

--- js/libs/PhaserGauge.js
+++ js/libs/PhaserGauge.js
@@ -1,20 +1,24 @@
 //Draw a Gauge
-//config is a dictionnary {background_color, color, x, y, height, width}
+//config is a dictionnary {background_color, color, x, y, height, width, coeff}
 
 function Gauge(phaser, percentage, config) {
     var graphics = phaser.add.graphics();
+    var radius = config.height / 2;
+    var rect_width = config.width - config.height;
+    var circle_center = config.x + radius;
+    var percentage = Math.abs(percentage) % 101;
 
     //Drawing the gauge's background
     graphics.fillStyle(config.background_color);
 
-    graphics.fillRect(config.x + (config.height / 2), config.y, config.width - config.height, config.height);
-    graphics.fillCircle(config.x + (config.height / 2 ), config.y + (config.height / 2 ), config.height / 2);
-    graphics.fillCircle(config.x + (config.height / 2 ) + (config.width - config.height), config.y + (config.height / 2 ), config.height / 2);
+    graphics.fillRect(circle_center, config.y, config.width - config.height, config.height);
+    graphics.fillCircle(circle_center, config.y + radius, radius);
+    graphics.fillCircle(circle_center + rect_width, config.y + radius, radius);
 
     //Drawing the Gauge itself
     graphics.fillStyle(config.color);
 
-    graphics.fillRect(config.x + (config.height / 2), config.y + ((0.1 * config.height) / 2), (percentage/100) * ((config.width - config.height)), 0.9 * config.height);
-    graphics.fillCircle(config.x + (config.height / 2), config.y + (config.height / 2), (0.9 * config.height) / 2);
-    graphics.fillCircle(config.x + ((percentage/100) * ((config.width - config.height))) + ((config.height / 2)), config.y + (config.height / 2), (0.9 * config.height) / 2);
+    graphics.fillRect(circle_center, config.y + (((1 - config.coeff) * config.height) / 2), (percentage/100) * (rect_width), config.coeff * config.height);
+    graphics.fillCircle(circle_center, config.y + radius, config.coeff * config.height / 2);
+    graphics.fillCircle(config.x + ((percentage/100) * rect_width) + radius, config.y + radius, config.coeff * config.height / 2);
 }
