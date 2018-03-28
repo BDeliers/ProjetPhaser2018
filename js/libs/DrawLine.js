e62afd0c5b96a2d519295ff9eae4f512c8cf4ad2
Duval-Corentin <corentin.duval9@gmail.com>
2018-03-28 14:48:24 +0200

    add new class for drawing line

--- js/libs/DrawLine.js
+++ js/libs/DrawLine.js
@@ -1,33 +1,59 @@
 /**
  * Draw a line between the set of points 
- * @function DrawLine
+ * @class Line : Wrapper of Phaser functions to draw line with differents params 
  * @param {Phaser} phaser  a instance of a Phaser object
- * @param {json} options {color : "0xHEX", width: pixel_width, rounded_angles: boolean}
  * @param {array} points a 2D array with the coordonate of each point as : [[x0, y0], [x1, y1], [xn, yn]]
  */
   
  console.log("Load DrawLine");
  
- function DrawLine(phaser, options, points){
-     // add a new graphics object
-     var graphics = phaser.add.graphics({lineStyle: { width: options.width, color: options.color}, fillStyle: {color: options.color} });
+class Line{
+     /**
+      * @method constructor Create a new instance of Line with initials params 
+      * @param {Phaser} phaser a instance of a Phaser Object 
+      * @param {json} options {color : "0xHEX", width: pixel_width, rounded_angles: boolean}
+      */
+    constructor(phaser, options){
+        // add a new graphics object
+        this.options = options;
+        this.phaser = phaser;
+        this.graphics = this.phaser.add.graphics({lineStyle: { width: this.options.width, color: this.options.color}, fillStyle: {color: this.options.color} });
+    }
 
-     console.log("ok");
-     // draw the first circle (out of index for for loop)
-     if(options.rounded_angles === true){
-         graphics.fillCircleShape(new Phaser.Geom.Circle(points[0][0], points[0][0], options.width / 2));
-     }
-     if(points.length >= 2){
-         console.log("ok2");
-        for(var i = 1; i < points.length; i++){
-            // draw the line
-           graphics.strokeLineShape( new Phaser.Geom.Line(points[i - 1][0], points[i - 1][1], points[i][0], points[i][1]) );
-            // draw circle for rounded shape if needed 
-           if(options.rounded_angles === true){
-               graphics.fillCircleShape( new Phaser.Geom.Circle(points[i][0], points[i][1], options.width / 2) );
-           }
+    /**
+     * @method draw Draw the line following a array of coordonates  
+     * @param {array} points a 2D array with the coordonate of each point as : [[x0, y0], [x1, y1], [xn, yn]]
+     */
+    draw(points){
+        // draw the first circle (out of index for for loop)
+        if(this.options.rounded_angles === true){
+            this.graphics.fillCircleShape(new Phaser.Geom.Circle(points[0][0], points[0][0], this.options.width / 2));
         }
-     }
-     
+        if(points.length >= 2){
+            for(var i = 1; i < points.length; i++){
+                // draw the line
+                this.graphics.strokeLineShape( new Phaser.Geom.Line(points[i - 1][0], points[i - 1][1], points[i][0], points[i][1]) );
+                // draw circle for rounded shape if needed 
+                if(this.options.rounded_angles === true){
+                    this.graphics.fillCircleShape( new Phaser.Geom.Circle(points[i][0], points[i][1], this.options.width / 2) );
+                }
+            }
+        }
+    }
 
- }
+    /**
+     * @method clean remove the current line of the Phaser Canvas and re-initialize a Phaser graphics object
+     */
+    clean(){
+        this.graphics.destroy();
+        this.graphics = this.phaser.add.graphics({lineStyle: { width: this.options.width, color: this.options.color}, fillStyle: {color: this.options.color} });
+    }
+
+    /**
+     * @method setOptions re-set new options for lines
+     * @param {json} options 
+     */
+    setOptions(options){
+        this.options = options;
+    }
+ };
