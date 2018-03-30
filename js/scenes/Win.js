4379969da00efe4ecc7a6384f1ac07f0e62a7eb0
“BDeliers” <contact@bdeliers.com>
2018-03-30 15:58:51 +0200

    Created scenes files

---
+++ js/scenes/Win.js
@@ -0,0 +1,19 @@
+define(["Phaser"], function(Phaser) {
+
+	console.log("Load scenes/Win");
+
+	var win_scene = {
+		preload: function (){
+
+				},
+		create: function (){
+
+				},
+		update: function (){
+
+				}
+	}
+
+	return win_scene;
+
+});
