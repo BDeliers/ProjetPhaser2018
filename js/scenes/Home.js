3579f6a2c6e073b3565c1f32e47c35a4fe517260
“BDeliers” <contact@bdeliers.com>
2018-03-30 16:28:09 +0200

    Tried to use sprite for the button on Home page

--- js/scenes/Home.js
+++ js/scenes/Home.js
@@ -3,27 +3,33 @@ define(["Phaser", "core/Button"], function(Phaser, Button) {
 	console.log("Load scenes/Home");
 
 	var home_scene = {
+
 		preload: function (){
-					  this.load.image('home', 'image/background/home.png');
+					this.load.image('home', 'image/background/home.png');
 
-					  var start_sprite = this.load.spritesheet('start_sprite',
-						  'image/buttons/earth.png',
-						  { frameWidth: 300, frameHeight: 300 }
-						);
+					this.load.spritesheet('start_sprite',
+						'image/buttons/earth.png',
+						{ frameWidth: 300, frameHeight: 300 }
+					);
 
-					  this.anims.create({
-							key:'commencer_0',
-							frames: this.anims.generateFrameNumbers("start_sprite", { start: -1, end: 0})
-						});
-						this.anims.create({
-							key:'commencer_1',
-							frames: this.anims.generateFrameNumbers("start_sprite", { start: 0, end: 1})
-						});
-					},
+					this.anims.create({
+						key:'commencer_0',
+						frames: this.anims.generateFrameNumbers("start_sprite", { start: -1, end: 0})
+					});
+					this.anims.create({
+						key:'commencer_1',
+						frames: this.anims.generateFrameNumbers("start_sprite", { start: 0, end: 1})
+					});
+				},
 		create: function (){
 				  this.add.image(480,300, 'home').setDisplaySize(960,600);
+				  var start_sprite = this.add.sprite(480, 300, "start_sprite");
 
-				  var button_start = new Button(this, "start_sprite", 480, 300);
+				  console.log(start_sprite);
+
+				  start_sprite.anims.play("commencer_0", true);
+
+				  /*var button_start = new Button(this, "start_sprite", 480, 300);
 				  button_start.on("hover_on", ()=>{
 					  start_sprite.anims.play("commencer_1", true);
 				  });
@@ -35,7 +41,7 @@ define(["Phaser", "core/Button"], function(Phaser, Button) {
 				  });
 				  button_start.on("click_off", ()=>{
 					  console.log("click off button");
-				  });
+				  });*/
 
 				},
 		update: function (){
