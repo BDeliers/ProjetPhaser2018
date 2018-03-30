134980589f38c5257fdadd030bf341cb3ca30c93
“BDeliers” <contact@bdeliers.com>
2018-03-30 15:46:30 +0200

    Added home scene

--- js/main.js
+++ js/main.js
@@ -19,62 +19,17 @@ require.config({
 });
 
 // Load Application
-require(["Phaser", "core/Button", "core/Clock", "core/PhaserGauge", "models/ScenarioModel"],
-function(Phaser, Button, Clock, Gauge, Scenario) {
+require(["Phaser", "core/Button", "core/Clock", "core/PhaserGauge", "models/ScenarioModel", "scenes/Home"],
+function(Phaser, Button, Clock, Gauge, Scenario, Home) {
 
 	var config = {
         type: Phaser.AUTO,
         width: 960,
         height: 600,
-        scene: {
-            preload: preload,
-            create: create,
-            update: update
-        }
+        scene: Home
     };
 
     var game = new Phaser.Game(config);
-
-    function preload (){
-      this.load.image('home', 'image/background/home.png');
-
-      var start_sprite = this.load.spritesheet('start_sprite',
-          'image/buttons/earth.png',
-          { frameWidth: 300, frameHeight: 300 }
-      	);
-
-      this.anims.create({
-    		key:'commencer_0',
-    		frames: this.anims.generateFrameNumbers("start_sprite", { start: -1, end: 0})
-    	});
-    	this.anims.create({
-    		key:'commencer_1',
-    		frames: this.anims.generateFrameNumbers("start_sprite", { start: 0, end: 1})
-    	});
-    }
-
-    function create (){
-      this.add.image(480,300, 'home').setDisplaySize(960,600);
-
-      var button_start = new Button(this, "start_sprite", 480, 300);
-      button_start.on("hover_on", ()=>{
-          start_sprite.anims.play("commencer_1", true);
-      });
-      button_start.on("hover_off", ()=>{
-          start_sprite.anims.play("commencer_0", true);
-      });
-      button_start.on("click_on", ()=>{
-          console.log("click on button");
-      });
-      button_start.on("click_off", ()=>{
-          console.log("click off button");
-      });
-
-    }
-
-    function update (){
-
-    }
 
     console.log('Load application');
 
