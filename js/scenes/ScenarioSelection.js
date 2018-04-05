323b7710f398a106a0e70f7240fe55e942fcf376
BDeliers <contact@bdeliers.com>
2018-04-05 17:35:16 +0200

    Finished the scenario choice page

--- js/scenes/ScenarioSelection.js
+++ js/scenes/ScenarioSelection.js
@@ -6,40 +6,44 @@ define(["Phaser", "core/Button"], function(Phaser, Button) {
 		preload: function (){
 					this.load.image('game', 'image/background/game.png');
 
-					this.load.spritesheet('scenar1_sprite',
-						'image/scenario/scenar1.png',
-						{ frameWidth: 300, frameHeight: 200 }
-					);
-
+					for (var i = 1; i < 7; i++) {
+						this.load.spritesheet('scenar'+i+'_sprite',
+							'image/scenario/scenar'+i+'.png',
+							{ frameWidth: 300, frameHeight: 200 }
+						);
+					}
 				},
 		create: function (){
 		 			this.add.image(480,300, 'game').setDisplaySize(960,600);
-					var scenar1_sprite = this.add.sprite(200, 200, "scenar1_sprite");
+					var scenar_sprites = [];
+					var origin = [225,225];
 
-					this.anims.create({
-						key:'scenar1_0',
-						frames: this.anims.generateFrameNumbers("scenar1_sprite", { start: -1, end: 0})
-					});
-					this.anims.create({
-						key:'scenar1_1',
-						frames: this.anims.generateFrameNumbers("scenar1_sprite", { start: 0, end: 1})
-					});
+					for (let i = 1; i < 7; i++) {
+						scenar_sprites.push(this.add.sprite(origin[0], origin[1], "scenar"+i+"_sprite").setDisplaySize(250, 166.6).setInteractive());
 
-					var button_start = new Button(this, "scenar1_sprite", 200, 200);
-					button_start.on("hover_on", ()=>{
-						scenar1_sprite.anims.play("scenar1_1", true);
-					});
-					button_start.on("hover_off", ()=>{
-						scenar1_sprite.anims.play("scenar1_0", true);
-					});
-					button_start.on("click_on", ()=>{
+						this.anims.create({
+							key:'scenar'+i+'_0',
+							frames: this.anims.generateFrameNumbers("scenar"+i+"_sprite", { start: -1, end: 0})
+						});
+						this.anims.create({
+							key:'scenar'+i+'_1',
+							frames: this.anims.generateFrameNumbers("scenar"+i+"_sprite", { start: 0, end: 1})
+						});
 
-					});
-					button_start.on("click_off", ()=>{
-						this.scene.start("Game");
-						this.scene.stop("ScenarioSelection");
-					});
+						scenar_sprites[i-1].on("pointerover", ()=>{scenar_sprites[i-1].anims.play("scenar"+i+"_1", true);});
+						scenar_sprites[i-1].on("pointerout", ()=>{scenar_sprites[i-1].anims.play("scenar"+i+"_0", true)});
+						scenar_sprites[i-1].on("pointerdown", ()=>{
+							this.scene.start("Game");
+	  					  	this.scene.stop("ScenarioSelection");
+						});
 
+						origin[0] += 250;
+
+						if (i == 3) {
+							origin[1] = 391;
+							origin[0] = 225;
+						}
+					}
 				},
 		update: function (){
 
