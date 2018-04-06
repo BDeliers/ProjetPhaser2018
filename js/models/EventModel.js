cd66195b21ef1f44ad43ebf224010bcd2e931fc3
Duval-Corentin <corentin.duval9@gmail.com>
2018-04-06 14:24:57 +0200

    dev EventModel (TODO: debug)

--- js/models/EventModel.js
+++ js/models/EventModel.js
@@ -1,7 +1,6 @@
 // Models Class interface for event JSON object
 
 // Event is a JS word
-
 define(["jquery"], function($) {
 
 	console.log("Load models/EventModel");
@@ -35,7 +34,6 @@ define(["jquery"], function($) {
 
 	Events.prototype.addRandomEvent() = function(){
 		const choose_index = Math.floor( (Math.random() * (this.events_json_list.length - 1) + 1));
-
 		if(this.active_events(this.events_json_list[choose_index]) == undefined){
 			this.active_events.push(this.events_json_list[choose_index]);
 			return true;
@@ -44,27 +42,64 @@ define(["jquery"], function($) {
 	}
 
 	Events.prototype.removeEvent = function(event_name){
-
+		for(let event of this.active_events){
+			if(event_name == event.name){
+				this.active_events.splice(this.active_events.indexOf(event), 1);
+				return true;
+			}
+		}
+		return false;
 	}
 
 	Events.prototype.isBlocked = function(vehicle_name){
-
+		for(let event of this.active_events){
+			for(let blocked_vehicle of event.blocked_vehicles){
+				if(vehicle_name == blocked_vehicle){
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	Events.prototype.getPolltionPertubativeCoeff = function(vehicle_name){
-
+		var pollution_perturbative_coeff = 1;
+		for(let event of this.active_events){
+			if(event.apply_to.find(vehicle_name) != undefined){
+				pollution_perturbativecoeff *= event.perturbative_pollution_coeff;
+			}
+		}
+		return pollution_perturbative_coeff;
 	}
 
 	Events.prototype.getMoneyPerturbativeCoeff = function(vehicle_name){
-
+		var money_perturbative_coeff = 1;
+		for(let event of this.active_events){
+			if(event.apply_to.find(vehicle_name) != undefined){
+				money_perturbative_coeff *= event.money_pollution_coeff;
+			}
+		}
+		return money_perturbative_coeff;
 	}
 
 	Events.prototype.getTimePerturbativeCoeff = function(vehicle_name){
-
+		var time_perturbative_coeff = 1;
+		for(let event of this.active_events){
+			if(event.apply_to.find(vehicle_name) != undefined){
+				time_perturbative_coeff *= event.time_pollution_coeff;
+			}
+		}
+		return time_perturbative_coeff;
 	}
 
 	Events.prototype.getExhaustPerturbativeCoeff = function(vehicle_name){
-
+		var exhaust_perturbative_coeff = 1;
+		for(let event of this.active_events){
+			if(event.apply_to.find(vehicle_name) != undefined){
+				exhaust_perturbative_coeff *= event.exhaust_pollution_coeff;
+			}
+		}
+		return exhaust_perturbative_coeff;
 	}
 
 	return Events;
