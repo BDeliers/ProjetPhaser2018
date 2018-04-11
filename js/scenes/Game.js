11dcac7280bb1299f897f49fc7fef21e8f38fea4
JDANEL <julie.danel@isen.yncrea.fr>
2018-04-11 13:54:00 +0200

    add bubble_sprite in Game

--- js/scenes/Game.js
+++ js/scenes/Game.js
@@ -9,6 +9,11 @@ define(["Phaser", "core/Clock", "models/ScenarioModel"], function(Phaser, Clock,
 						{ frameWidth: 60, frameHeight: 100 }
 					);
 
+					this.load.spritesheet('bubble_sprite',
+						'image/characters/text_bubble/bubble_sprite.png',
+						{ frameWidth: 214, frameHeight: 216}
+					);
+
 					var scenario_model = new Scenario(this, document.cookie.split('=')[1]);
 
 					this.load.image('game', "image/background/game.png");
@@ -22,16 +27,41 @@ define(["Phaser", "core/Clock", "models/ScenarioModel"], function(Phaser, Clock,
 					this.add.image(500,680, 'bottom').setDisplaySize(1012,150);
 					this.add.image(500,50, 'top').setDisplaySize(1012,100);
 
-					var clock = new Clock(this, "clock_sprite", 100, 100);
-
+					var clock = new Clock(this, "clock_sprite", 1100, 50);
 					var seconds = 0;
+					var bubble_sprite = this.add.sprite(1000, 300, "bubble_sprite");
 
-					var schedule_task = setInterval(()=>{
-						seconds += 1;
-						clock.set_seconds(seconds);
-						clock.update();
-					}, 1000);
+				 this.anims.create({
+					 key:'bubble_orange',
+					 frames: this.anims.generateFrameNumbers("bubble_sprite", { start: -1, end: 0})
+				 });
+				 this.anims.create({
+					 key:'bubble_green',
+					 frames: this.anims.generateFrameNumbers("bubble_sprite", { start: 0, end: 1})
+				 });
+				 this.anims.create({
+					 key:'bubble_red',
+					 frames: this.anims.generateFrameNumbers("bubble_sprite", { start: 1, end: 2})
+				 });
+				 this.anims.create({
+					 key:'bubble_gray',
+					 frames: this.anims.generateFrameNumbers("bubble_sprite", { start: 2, end: 3})
+				 });
+				 this.anims.create({
+					 key:'bubble_blue',
+					 frames: this.anims.generateFrameNumbers("bubble_sprite", { start: 3, end: 4})
+				 });
+
+				 bubble_sprite.anims.play("bubble_orange", true);
+
+				 var schedule_task = setInterval(()=>{
+					 seconds += 1;
+					 clock.set_seconds(seconds);
+					 clock.update();
+				 }, 1000);
+
 				},
+
 		update: function (){
 
 				}
